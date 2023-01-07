e6f423ae334cb68ba6345a0e97848d822d264d8e
shibinpoolat <geniuseasysa@gmail.com>
2023-01-07 14:26:12 +0300

    cores 6

--- app/app/settings.py
+++ app/app/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = []
 CORS_ALLOWED_ORIGINS.extend(
     filter(
         None,
